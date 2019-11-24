3df07f5d2d3973d7e7cd89307384a591fcbd024f
Edmund <matrim.eg@gmail.com>
2019-11-24 15:22:31 +0100

    Dynamic cards, added content json

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { cards } from "./../../content"
 
 import Layout from "../components/layout"
 import Card from "../components/card"
@@ -9,20 +10,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Bunch of Biohacks" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Card
-        category="Pendeln"
-        title="Podcast Folge Deutschlandfunk"
-        short="Eine Folge Deutschlandfunk 'Der Hintergrund' hat ca. 20 Minuten Laufzeit. Perfekt für den Weg zur Arbeit."
-        effort="1"
-        gain="2"
-      />
-      <Card
-        category="Pendeln"
-        title="Sprache lernen mit Memrise"
-        short="Mit den Öffis auf dem Weg zur Arbeit oder nach Hause? Dann hängst du wohl eh am Smartphone. Nutze die Zeit um die Sprachkenntnisse zu erweitern."
-        effort="2"
-        gain="4"
-      />
+      {cards.map(card => (
+        <Card {...card} />
+      ))}
     </div>
   </Layout>
 )
